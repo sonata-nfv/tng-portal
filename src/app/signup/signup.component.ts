101ac590ca0811c820ad2e4ac42733ae922bc0cf
anapolg <ana.pol@quobis.com>
2018-02-12 15:35:21 +0100

    Minor changes to reduce logic in register html

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -14,6 +14,7 @@ export class SignupComponent implements OnInit {
   userErrorString: string;
   passwordErrorString: string;
   emailErrorString: string = "This is not a valid email.";
+  validPassword: boolean;
 
   signupForm: FormGroup;
 
@@ -39,8 +40,10 @@ export class SignupComponent implements OnInit {
     if (values.confirmPassword !== null) {
       if (values.password !== values.confirmPassword) {
         this.passwordErrorString = "*Passwords do not match.";
+        this.validPassword = false;
       } else {
         this.passwordErrorString = null;
+        this.validPassword = true;
       }
     }
   }
