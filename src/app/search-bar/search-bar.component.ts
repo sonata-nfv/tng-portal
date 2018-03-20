b59e4558b6e31e1aa7e1e5a79ffd83d001c5245f
anapolg <ana.pol@quobis.com>
2018-03-20 22:32:39 +0100

    Fixed border bottom and input color in search bar

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchBarComponent implements OnInit {
   searchText: string;
