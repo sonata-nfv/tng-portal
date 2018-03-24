308de7176f61fd81ce5acb2832cd00b9046209ca
anapolg <ana.pol@quobis.com>
2018-03-24 12:46:40 +0100

    Adding routing to NS detailed view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,17 @@ import { AuthGuard } from "./auth-guard";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
 import { RegisteredComponent } from "./registered/registered.component";
-import { IndexComponent } from './index/index.component';
+import { IndexComponent } from "./index/index.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { UsersComponent } from './users/users.component';
-import { ValidationComponent } from './validation/validation.component';
-import { ServicePlatformComponent } from './service-platform/service-platform.component';
-import { AvailableNetworkServicesComponent } from './available-network-services/available-network-services.component';
-import { NetworkServiceComponent } from './network-service/network-service.component';
-import { RequestsComponent } from './requests/requests.component';
-import { NetworkServiceInstancesComponent } from './network-service-instances/network-service-instances.component';
-import { ServiceLicencesComponent } from './service-licences/service-licences.component';
-import { UserLicencesComponent } from './user-licences/user-licences.component';
+import { UsersComponent } from "./users/users.component";
+import { ValidationComponent } from "./validation/validation.component";
+import { ServicePlatformComponent } from "./service-platform/service-platform.component";
+import { AvailableNetworkServicesComponent } from "./available-network-services/available-network-services.component";
+import { NetworkServiceComponent } from "./network-service/network-service.component";
+import { RequestsComponent } from "./requests/requests.component";
+import { NetworkServiceInstancesComponent } from "./network-service-instances/network-service-instances.component";
+import { ServiceLicencesComponent } from "./service-licences/service-licences.component";
+import { UserLicencesComponent } from "./user-licences/user-licences.component";
 
 const routes: Routes = [
   // Redirect to login while there is no dashboard/menu to display
@@ -24,22 +24,29 @@ const routes: Routes = [
   { path: "signup", component: SignupComponent },
   { path: "registered", component: RegisteredComponent },
   { path: "portal", redirectTo: "index", pathMatch: "full" },
-  { path: "", component: IndexComponent, canActivate: [AuthGuard], children: [
-    { path: "", redirectTo: "dashboard", pathMatch: "full" },
-    { path: "dashboard", component: DashboardComponent },
-    { path: "users", component: UsersComponent },
-    { path: "validation", component: ValidationComponent },
-    // Service Platform section
-    { path: "servicePlatform", component: ServicePlatformComponent },
-    // Service Management section
-    { path: "availableNetworkServices", component: AvailableNetworkServicesComponent, children: [
-      { path: "networkService", component: NetworkServiceComponent }
-    ] },
-    { path: "requests", component: RequestsComponent },
-    { path: "instances", component: NetworkServiceInstancesComponent },
-    { path: "serviceLicences", component: ServiceLicencesComponent },
-    { path: "userLicences", component: UserLicencesComponent },
-  ]}
+  {
+    path: "",
+    component: IndexComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "dashboard", component: DashboardComponent },
+      { path: "users", component: UsersComponent },
+      { path: "validation", component: ValidationComponent },
+      // Service Platform section
+      { path: "servicePlatform", component: ServicePlatformComponent },
+      // Service Management section
+      {
+        path: "availableNetworkServices",
+        component: AvailableNetworkServicesComponent,
+        children: [{ path: "detail/:id", component: NetworkServiceComponent }]
+      },
+      { path: "requests", component: RequestsComponent },
+      { path: "instances", component: NetworkServiceInstancesComponent },
+      { path: "serviceLicences", component: ServiceLicencesComponent },
+      { path: "userLicences", component: UserLicencesComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/network-service/network-service.component.ts
+++ src/app/network-service/network-service.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 import { ServiceManagementService } from "../shared/services/serviceManagement/serviceManagement.service";
 
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-network-service',
-  templateUrl: './network-service.component.html',
-  styleUrls: ['./network-service.component.scss'],
+  selector: "app-network-service",
+  templateUrl: "./network-service.component.html",
+  styleUrls: ["./network-service.component.scss"],
   encapsulation: ViewEncapsulation.None
 })
 export class NetworkServiceComponent implements OnInit {
-  uuid: string;
   name: string;
   author: string;
   version: string;
@@ -20,30 +19,38 @@ export class NetworkServiceComponent implements OnInit {
   type: string;
   description: string;
 
-  constructor(private serviceManagementService: ServiceManagementService, private router: Router) { }
+  constructor(
+    private serviceManagementService: ServiceManagementService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    // GET uuid from URL
+    this.route.params.subscribe(params => {
+      let uuid = params["id"];
 
-    this.serviceManagementService.getNetworkService(this.uuid).then((response) => {
-      // Take the params desired and pass them to HTML
-
-      console.log(response);
-    }).catch(err => {
-      console.error(err);
+      this.serviceManagementService
+        .getNetworkService(uuid)
+        .then(response => {
+          this.name = response.nsd.name;
+          this.author = response.nsd.author;
+          this.version = response.nsd.version;
+          this.vendor = response.nsd.vendor;
+          this.serviceID = response.uuid;
+          this.type = response.user_licence;
+          this.description = response.nsd.description;
+          return;
+        })
+        .catch(err => {
+          console.error(err);
+        });
     });
   }
 
   // TODO instance from network service displayed
-  instanciate() {
-    console.log("TODO instanciate");
-  }
+  instanciate() {}
 
   close() {
-    // TODO remove id from URL
     this.router.navigate(["/availableNetworkServices"]);
   }
-
-
-
 }
