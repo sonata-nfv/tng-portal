4c90976e889dc6f03eab1ab24e4d35ab8a89c772
danifernandezcalv <daniel.fernandez@quobis.es>
2018-04-18 10:08:20 +0200

    Added licences and licences-detail

--- src/app/licences/licences.component.ts
+++ src/app/licences/licences.component.ts
@@ -5,7 +5,7 @@ import { Router, ActivatedRoute } from "@angular/router";
 
 import { ServiceManagementService } from "../shared/services/service-management/serviceManagement.service";
 import { DialogDataService } from "../shared/services/dialog/dialog.service";
-
+import { DataTransferService } from "../shared/services/service-management/dataTransfer.service";
 
 @Component({
   selector: "app-licences",
@@ -15,48 +15,52 @@ import { DialogDataService } from "../shared/services/dialog/dialog.service";
 })
 export class LicencesComponent implements OnInit {
   loading: boolean;
-  licenses: Array<Object>;
+  licences = new Array();
   dataSource = new MatTableDataSource();
-  displayedColumns = [
-    "Status",
-    "Licence ID",
-    "Related Service",
-    "Type",
-    "buy"
-  ];
+  displayedColumns = ["Status", "Licence ID", "Related Service", "Type", "buy"];
   searchText: string;
 
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private dataTransfer: DataTransferService
   ) {}
 
   ngOnInit() {
     this.loading = false;
-    this.licenses = [{
-      searchField: 'uno',
-      status: 'active',
-      licenceId: '11fff5fa-5770-4fe7-9e34-a0f60ae63b88',
-      relatedService: "9f9213c9-1134-43bd-9351-50bff41765de",
-      type: 'public'
-    },{
-      searchField: 'dos',
-      status: 'GHLJHG',
-      licenceId: '21fff5fa-5770-4fe7-9e34-a0f60ae63b88',
-      relatedService: "2f9213c9-1134-43bd-9351-50bff41765de",
-      type: 'public'
-    }];
+    this.licences = [
+      {
+        searchField: "uno",
+        status: "active",
+        licenceId: "11fff5fa-5770-4fe7-9e34-a0f60ae63b88",
+        relatedService: "9f9213c9-1134-43bd-9351-50bff41765de",
+        type: "public",
+        description:
+          "Description of service 1: fsdfhgsiduafgs aiud fgisuadf guisigaisd safsufhsu fuisfhhgf sdgfgiugii uigiagfg sdigf"
+      },
+      {
+        searchField: "dos",
+        status: "GHLJHG",
+        licenceId: "21fff5fa-5770-4fe7-9e34-a0f60ae63b88",
+        relatedService: "2f9213c9-1134-43bd-9351-50bff41765de",
+        type: "public",
+        description: "Description of service 2"
+      }
+    ];
     for (let i = 0; i < 5; i++) {
-      this.licenses = this.licenses.concat(
-        this.licenses
-      );
+      this.licences = this.licences.concat(this.licences);
     }
-
   }
 
   receiveMessage($event) {
     this.searchText = $event;
   }
-  
-  buy(row) {
-
+  openLicences(row) {
+    let uuid = row.licenceId;
+    let detail = this.licences.find(x => x.licenceId === uuid);
+    this.dataTransfer.sendDetail(detail);
+    this.router.navigate(["detail/", uuid], { relativeTo: this.route });
   }
+
+  buy(row) {}
 }
