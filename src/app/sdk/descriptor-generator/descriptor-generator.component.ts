20cb037a19c378c54c9334f296ce7d65f225046e
Zulfiqar Ahmed <zulfiqarjunejo@live.com>
2019-03-29 14:19:13 +0100

    fix(descriptor-generator): changed style file extension to .scss

--- src/app/sdk/descriptor-generator/descriptor-generator.component.ts
+++ src/app/sdk/descriptor-generator/descriptor-generator.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-descriptor-generator',
   templateUrl: './descriptor-generator.component.html',
-  styleUrls: ['./descriptor-generator.component.css']
+  styleUrls: ['./descriptor-generator.component.scss']
 })
 export class DescriptorGeneratorComponent implements OnInit {
 
