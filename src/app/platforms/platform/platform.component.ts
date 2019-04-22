d8ec9b011d0fe03a1b04a7435b6aedeaa677f517
Ana <ana.pol@quobis.com>
2019-04-22 14:38:37 +0200

    feat(platforms): included vim_account field for OSM
    Included new field vim_account for OSM type platforms.
    
    Fixes #296

--- src/app/platforms/platform/platform.component.ts
+++ src/app/platforms/platform/platform.component.ts
@@ -49,8 +49,10 @@ export class PlatformComponent implements OnInit {
 			password: new FormControl('', Validators.required),
 			monitoring_urls: new FormControl()
 		});
+
 		this.osmForm = new FormGroup({
-			project_name: new FormControl('', Validators.required)
+			project_name: new FormControl('', Validators.required),
+			vim_account: new FormControl('', Validators.required)
 		});
 	}
 
@@ -66,6 +68,7 @@ export class PlatformComponent implements OnInit {
 		this.platformForm.get('password').setValue(this.originalPlatform[ 'password' ]);
 		this.platformForm.get('monitoring_urls').setValue(this.originalPlatform[ 'monitoring_urls' ]);
 		this.osmForm.get('project_name').setValue(this.originalPlatform[ 'project_name' ]);
+		this.osmForm.get('vim_account').setValue(this.originalPlatform[ 'vim_account' ]);
 	}
 
 	private async requestPlatform(uuid) {
@@ -115,10 +118,11 @@ export class PlatformComponent implements OnInit {
 			host: this.platformForm.get('host').value,
 			username: this.platformForm.get('username').value,
 			password: this.platformForm.get('password').value,
-			monitoring_urls: this.platformForm.get('monitoring_urls').value,
+			monitoring_urls: this.platformForm.get('monitoring_urls').value || '',
 		};
 		if (this.osmForm.get('project_name').value) {
 			platform[ 'project_name' ] = this.osmForm.get('project_name').value;
+			platform[ 'vim_account' ] = this.osmForm.get('vim_account').value;
 		}
 		if (this.edition && this.originalPlatform[ 'service_token' ]) {
 			platform[ 'service_token' ] = this.originalPlatform[ 'service_token' ];
