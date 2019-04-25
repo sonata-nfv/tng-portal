edc3ce7e70b88fe8d9ebfef5c714ddb3b40413e8
Ana <ana.pol@quobis.com>
2019-04-25 18:46:45 +0200

    feat(dashboard): first draft
    CPU/memory graphs for platform and vm
    
    Fixes #280

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-dashboard',
@@ -7,7 +8,32 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 	encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit {
-	constructor() { }
+	minutes = 30;
+	refreshRate = '5s';
+
+	constructor(private sanitizer: DomSanitizer) {
+	}
 
 	ngOnInit() { }
+
+	getDate() {
+		return new Date().getTime();
+	}
+
+	getMinutesAgo(minutes) {
+		return new Date().getTime() - (minutes * 60);
+	}
+
+	getGraphUrl(panelId) {
+		return this.sanitizer.bypassSecurityTrustResourceUrl(`http://pre-int-sp-ath.5gtango.eu:3000/d-solo/SP_dash1/sp?orgId=1&` +
+			`panelId=${ panelId }&` +
+			`from=getFifteenMinutesAgo()&` +
+			`to=getDate()&` +
+			`var-id=341f4d56-8e66-cfae-6fb3-1143fff11091&` +
+			`var-entity=vm&` +
+			`var-env=pre-int-sp%3A341f4d56-8e66-cfae-6fb3-1143fff11091&` +
+			`theme=light&` +
+			`refresh=${ this.refreshRate }`);
+	}
+
 }
