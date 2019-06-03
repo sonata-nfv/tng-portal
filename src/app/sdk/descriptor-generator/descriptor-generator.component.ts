52f343196abd293339ec1fe63883c5eee3c7e681
Zulfiqar Ahmed <zulfiqarjunejo@live.com>
2019-06-03 15:02:42 +0200

    feat(sdk): added functionality to send POST request to create a new project

--- src/app/sdk/descriptor-generator/descriptor-generator.component.ts
+++ src/app/sdk/descriptor-generator/descriptor-generator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ControlsValidator } from '../../shared/utils/controls-validator';
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
 	selector: 'app-descriptor-generator',
@@ -16,7 +17,8 @@ export class DescriptorGeneratorComponent implements OnInit {
 	isEmpty = true;
 	section = 'sdk';
 
-	constructor(private router: Router, private route: ActivatedRoute, private controlsValidator: ControlsValidator) { }
+	constructor(private router: Router, private route: ActivatedRoute, private controlsValidator: ControlsValidator,
+		private http: HttpClient) { }
 
 	ngOnInit() {
 		this.initForm();
@@ -39,13 +41,23 @@ export class DescriptorGeneratorComponent implements OnInit {
 	}
 
 	createService() {
-		console.log({
-			name: this.serviceForm.get('name').value,
-			author: this.serviceForm.get('author').value,
-			vendor: this.serviceForm.get('vendor').value,
-			description: this.serviceForm.get('description').value,
-			numberOfVNFs: this.serviceForm.get('numberOfVNFs')
-		});
+		const endpoint = 'http://localhost:5098/api/v1/projects';
+
+		const body = new HttpParams()
+			.set('name', this.serviceForm.get('name').value)
+			.set('author', this.serviceForm.get('author').value)
+			.set('vendor', this.serviceForm.get('vendor').value)
+			.set('description', this.serviceForm.get('description').value)
+			.set('vnfs', this.serviceForm.get('numberOfVNFs').value);
+
+		this.http.post(endpoint,
+			body.toString(),
+			{
+				headers: new HttpHeaders()
+					.set('Content-Type', 'application/x-www-form-urlencoded')
+					.set('Access-Control-Allow-Origin', 'http://localhost:5098')
+			}
+		).subscribe(response => console.log(response));
 	}
 
 	close() {
