db81e5d922145588f990cfbed486ddd53a5149f5
Ana <ana.pol@quobis.com>
2019-06-07 13:21:54 +0200

    fix(ns-instantiate-dialog): sending sla_uuid during license purchase
    If there are slas hide the None option. Besides, error fix including sla_uuid for license purchase.
    
    Fixes #342

--- src/app/service-management/ns-instantiate-dialog/ns-instantiate-dialog.component.ts
+++ src/app/service-management/ns-instantiate-dialog/ns-instantiate-dialog.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewEncapsulation, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { Router } from '@angular/router';
 
 import { ServiceManagementService } from '../service-management.service';
 import { UtilsService } from '../../shared/services/common/utils.service';
 import { CommonService } from '../../shared/services/common/common.service';
+import { DialogDataService } from '../../shared/services/dialog/dialog.service';
 
 @Component({
 	selector: 'app-ns-instantiate-dialog',
@@ -25,12 +25,12 @@ export class NsInstantiateDialogComponent implements OnInit {
 	slas: Array<any>;
 
 	constructor(
-		private router: Router,
 		private utilsService: UtilsService,
 		private commonService: CommonService,
 		public dialogRef: MatDialogRef<NsInstantiateDialogComponent>,
 		@Inject(MAT_DIALOG_DATA) public data: any,
-		private serviceManagementService: ServiceManagementService
+		private serviceManagementService: ServiceManagementService,
+		private dialogData: DialogDataService
 	) { }
 
 	ngOnInit() {
@@ -55,18 +55,21 @@ export class NsInstantiateDialogComponent implements OnInit {
 		this.loading = false;
 		if (templates) {
 			// GET SLA templates for this service
-			this.slas = templates.filter(x => x.nsUUID === this.data.serviceUUID).map(x => ({ uuid: x.uuid, name: x.name }));
+			this.slas = templates.filter(x => x.nsUUID === this.data.serviceUUID).map(x => ({ uuid: x.uuid, name: x.name, license: x.license }));
 			// If no SLA were created for the NS, there are no licenses associated
 			this.instantiationIsAllowed = this.slas.length ? false : true;
-			this.slas.unshift({ uuid: 'None', name: 'None' });
 		} else {
 			this.utilsService.openSnackBar('Unable to fetch SLA templates', '');
+		}
+
+		// If error or no SLA received for the service include None one
+		if (!this.slas || !this.slas.length) {
+			this.slas.unshift({ uuid: 'None', name: 'None' });
 		}
 
 		if (endpoints) {
 			this.locations = endpoints;
 			this.locations.unshift({ uuid: 'None', name: 'None' });
-
 		} else {
 			this.utilsService.openSnackBar('Unable to fetch locations', '');
 		}
@@ -97,28 +100,37 @@ export class NsInstantiateDialogComponent implements OnInit {
 
 	async receiveSLA(sla) {
 		if (sla && sla !== 'None') {
+			this.instantiationForm.get('sla').setValue(sla);
+
 			// Check if license is valid before instantiate
 			const response = await this.serviceManagementService.getLicenseStatus(sla, this.data.serviceUUID);
 
-			if (response && response[ 'allowed_to_instantiate' ]) {
-				// License allows the instantiation
-				this.instantiationForm.get('sla').setValue(sla);
-				this.instantiationIsAllowed = true;
-			} else {
-				// License does not allow instantiation
-				this.instantiationForm.get('sla').setValue('');
-				this.instantiationIsAllowed = false;
+			if (response) {
+				this.instantiationIsAllowed = response[ 'allowed_to_instantiate' ] ? true : false;
 
 				// License does not allow instantiation
 				// case license public or trial : case license private
-				response[ 'license_type' ] !== 'private' ?
-					this.section = 'error' :
-					this.section = 'buy';
+				if (!response[ 'allowed_to_instantiate' ]) {
+					response[ 'license_type' ] !== 'private' ?
+						this.section = 'error' :
+						this.section = 'buy';
+				}
+			} else {
+				this.instantiationIsAllowed = this.instantiationForm.get('sla').value[ 'license' ] === 'public' ?
+					true : false;
+
+				// If there is no response regarding license validity and the NS is not public then instantiation is forbidden
+				if (!this.instantiationIsAllowed) {
+					this.close();
+					const title = 'oh oh...';
+					const content = 'There was an error checking the validity of the license. As this network service is not public, you are not allowed \
+								to instantiate it for the moment. Please, try again later.';
+					const action = 'Accept';
+					this.dialogData.openDialog(title, content, action, () => { });
+				}
 			}
 		} else {
 			this.instantiationForm.get('sla').setValue('');
-			// If there are SLAs (more than the 'None' one) the NS is not public and instantiation is forbidden
-			this.instantiationIsAllowed = this.slas.length > 1 ? false : true;
 		}
 	}
 
