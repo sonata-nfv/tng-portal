bea5e2f0b8deb3453f68c3ad8904895514c22faf
Stefan Schneider <stefan.schneider@upb.de>
2019-08-21 19:23:01 +0200

    add packaging functionality in project-detail component

--- src/app/sdk/project-detail/project-detail.component.ts
+++ src/app/sdk/project-detail/project-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../Project';
 import { SdkService } from '../sdk.service';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
 	selector: 'app-project-detail',
@@ -11,8 +12,13 @@ export class ProjectDetailComponent implements OnInit {
 	// TODO: move this to a new environment in the environments folder and use angular configuration instead
 	baseip = 'http://192.168.99.100';
 	project: Project;
+	// packaging related variables
+	skipValidation = true;
+	pkgOutput = 'Packaging output. Packaging not yet started.';
+	pkgPath = '';
+	pkgSuccess = false;
 
-	constructor(private sdkService: SdkService) { }
+	constructor(private sdkService: SdkService, private http: HttpClient) { }
 
 	ngOnInit() {
 		this.project = this.sdkService.project;
@@ -21,5 +27,38 @@ export class ProjectDetailComponent implements OnInit {
 	// check if proper project is loaded or just the default UUID is selected
 	projectLoaded(): boolean {
 		return this.project.uuid !== 'defaultUuid';
+	}
+
+	package(): void {
+		const endpoint = this.baseip + ':5098/api/v1/projects/' + this.project.uuid + '/package';
+		const header = new HttpHeaders()
+			.set('Content-Type', 'application/x-www-form-urlencoded')
+			.set('Access-Control-Allow-Origin', this.baseip + ':5098');
+
+		// skip validation if selected (add mat-checkbox)
+		const body = new HttpParams().set('skip_validation', String(this.skipValidation));
+
+		// update icon to show progress circle
+		if (this.skipValidation) {
+			this.pkgOutput = 'Packaging without validation...';
+		} else {
+			this.pkgOutput = 'Valdiating and packaging...';
+		}
+
+		this.http.post(endpoint, body.toString(), { headers: header })
+			.subscribe(response => {
+				console.log(response);
+				if (response['error_msg'] == null) {
+					this.pkgOutput = 'Success';
+					this.pkgPath = response['package_path'];
+					this.pkgSuccess = true;
+				} else {
+					this.pkgOutput = response['error_msg'];
+				}
+			});
+	}
+
+	onboardPackage(): void {
+		console.log('On-boarding not yet implemented');
 	}
 }
