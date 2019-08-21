7e3537f4d8dc212a37321c7d996a5d01175be1e9
Ana <ana.pol@quobis.com>
2019-08-21 15:00:03 +0200

    fix(dashboard): remove from and to which time in graphs
    Fixes #280

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,8 +11,7 @@ import { CommonService } from '../shared/services/common/common.service';
 	encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-	minutes = 30;
-	refreshRateGraphs = '30s';
+	refreshRateGraphs = '10s';
 	// refreshRateRequests = 60000;
 	dashboardData = { };
 	uptime: string;
@@ -32,19 +31,9 @@ export class DashboardComponent implements OnInit, OnDestroy {
 		// clearTimeout(this.getDataTimeOut);
 	}
 
-	getDate() {
-		return new Date().getTime();
-	}
-
-	getMinutesAgo(minutes) {
-		return new Date().getTime() - (minutes * 60000);
-	}
-
 	getGraphUrl(panelId) {
 		return this.sanitizer.bypassSecurityTrustResourceUrl(`${ this.config.baseSP }${ this.config.graphs }/d-solo/sp_dash/sp?orgId=1&` +
 			`panelId=${ panelId }&` +
-			`from=${ this.getMinutesAgo(this.minutes) }&` +
-			`to=${ this.getDate() }&` +
 			`var-id=341f4d56-8e66-cfae-6fb3-1143fff11091&` +
 			`var-entity=vm&` +
 			`var-env=pre-int-sp%3A341f4d56-8e66-cfae-6fb3-1143fff11091&` +
