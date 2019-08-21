f7bf46c82bb0c0d6a3229f1eb6ade888ee4be457
Stefan Schneider <stefan.schneider@upb.de>
2019-08-21 13:29:49 +0200

    no-cache when building sdk portal image to always pull latest version of sdk tools from github

--- build-sdk-portal.sh
+++ build-sdk-portal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker build -f Dockerfile_sdkportal_backend -t sonatanfv/sdk-portal-backend .
+docker build --no-cache -f Dockerfile_sdkportal_backend -t sonatanfv/sdk-portal-backend .
