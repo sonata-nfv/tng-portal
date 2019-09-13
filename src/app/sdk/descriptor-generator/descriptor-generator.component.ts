99508eee1555b46d68b170f8f3e80e39428fda67
Stefan Schneider <stefan.schneider@upb.de>
2019-09-13 17:45:29 +0200

    fix: set http body params correctly; + clean up

--- src/app/sdk/descriptor-generator/descriptor-generator.component.ts
+++ src/app/sdk/descriptor-generator/descriptor-generator.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-// import { ControlsValidatorDirective } from '../../shared/utils/controls-validator';
 import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
 import { SdkService } from '../sdk.service';
 import { ConfigService } from '../../shared/services/config/config.service';
@@ -15,7 +13,6 @@ import { Project } from '../Project';
 })
 export class DescriptorGeneratorComponent implements OnInit {
 
-	serviceForm: FormGroup;
 	disabledButton = true;
 	section = 'sdk';
 	project = new Project();
@@ -24,43 +21,18 @@ export class DescriptorGeneratorComponent implements OnInit {
 				private config: ConfigService) { }
 
 	ngOnInit() {
-		this.initForm();
-	}
-
-	initForm() {
-		this.serviceForm = new FormGroup({
-			name: new FormControl(''),
-			author: new FormControl(''),
-			vendor: new FormControl(''),
-			description: new FormControl(''),
-			numberOfVNFs: new FormControl('', [Validators.required])
-		});
-		this.serviceForm.valueChanges.subscribe(() => this.onFormChanges());
-	}
-
-	private onFormChanges() {
-		this.disabledButton = !this.serviceForm.valid;
 	}
 
 	createService() {
 		const endpoint = this.config.baseSDK + ':5098/api/v1/projects';
 
 		// only generate Tango descriptors as the SP doesn't accept packages with Tango and OSM
-		const body = new HttpParams().set('only_tango', 'true');
-
-		// get info from form
-		// const name = this.serviceForm.get('name').value;
-		// const author = this.serviceForm.get('author').value;
-		// const vendor = this.serviceForm.get('vendor').value;
-		// const description = this.serviceForm.get('description').value;
-		// const numVnfs = this.serviceForm.get('numberOfVNFs').value;
-
-		// set values that are not empty
-		body.set('name', this.project.name);
-		body.set('author', this.project.author);
-		body.set('vendor', this.project.vendor);
-		body.set('description', this.project.description);
-		body.set('vnfs', String(this.project.numVnfs));
+		const body = new HttpParams().set('only_tango', 'true')
+			.set('name', this.project.name)
+			.set('author', this.project.author)
+			.set('vendor', this.project.vendor)
+			.set('description', this.project.description)
+			.set('vnfs', String(this.project.numVnfs));
 
 		this.http.post(endpoint,
 			body.toString(),
