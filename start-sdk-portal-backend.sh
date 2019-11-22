f6a58b4f583b53618db33a5234d807088024ca6a
Stefan Schneider <stefan.schneider@upb.de>
2019-11-22 17:02:02 +0100

    changed base url of jupyter lab

--- start-sdk-portal-backend.sh
+++ start-sdk-portal-backend.sh
@@ -3,4 +3,4 @@ set -e
 # start tng-sdk-project service in background
 tng-project -s -v &
 # start jupyter notebook and lab (access via /lab endpoint)
-jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token=''
+jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token='' --NotebookApp.base_url='/tng-lab'
