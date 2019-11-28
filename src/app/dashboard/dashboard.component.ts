fbd7843d78ff9274378c556ae5aa2f0ee3d5bc7d
Ana <ana.pol@quobis.com>
2019-11-28 14:17:19 +0100

    fix(dashboard): request var-env according to domain and vim uuid
    Fixes #466

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 import { ConfigService } from '../shared/services/config/config.service';
 import { CommonService } from '../shared/services/common/common.service';
+import { DashboardService } from './dashboard.service';
 
 @Component({
 	selector: 'app-dashboard',
@@ -15,37 +16,35 @@ export class DashboardComponent implements OnInit {
 	dashboardData = { };
 	uptime: string;
 	environment: string;
+	vimData: object;
+	metricName = 'vm_mem_perc';
 
 	constructor(
 		private sanitizer: DomSanitizer,
 		private config: ConfigService,
 		private commonService: CommonService,
-		private configService: ConfigService) { }
+		private configService: ConfigService,
+		private dashboardService: DashboardService,
+	) { }
 
 	ngOnInit() {
 		this.environment = this.configService.environment;
+		this.requestVimUUID();
 
 		this.environment === 'SP' ?
 			this.getSPDashboardData()
 			: this.getVNVDashboardData();
 	}
 
-	getSPGraphUrl(panelId) {
-		return this.sanitizer.bypassSecurityTrustResourceUrl(`${ this.config.baseSP }${ this.config.graphs }/d-solo/sp_dash/sp?orgId=1&` +
-			`panelId=${ panelId }&` +
-			`var-id=341f4d56-8e66-cfae-6fb3-1143fff11091&` +
-			`var-entity=vm&` +
-			`var-env=pre-int-sp%3A341f4d56-8e66-cfae-6fb3-1143fff11091&` +
-			`theme=light&` +
-			`refresh=${ this.refreshRateGraphs }`);
+	async requestVimUUID() {
+		this.vimData = await this.dashboardService.getVimUuid(this.metricName);
 	}
-
-	getVNVGraphUrl(panelId) {
-		return this.sanitizer.bypassSecurityTrustResourceUrl(`${ this.config.baseVNV }${ this.config.graphs }/d-solo/sp_dash/sp?orgId=1&` +
+	getGraphUrl(panelId) {
+		return this.sanitizer.bypassSecurityTrustResourceUrl(`${ this.config.base }${ this.config.graphs }/d-solo/sp_dash/sp?orgId=1&` +
 			`panelId=${ panelId }&` +
-			`var-id=4fd837f6-5f49-11dc-a9ed-000ea629289b&` +
+			`var-id=${ this.vimData[ 'vimUUID' ] }&` +
 			`var-entity=vm&` +
-			`var-env=pre-int-vnv-bcn.5gtango.eu%3A4fd837f6-5f49-11dc-a9ed-000ea629289b&` +
+			`var-env=${ this.vimData[ 'vimEnv' ] }&` +
 			`theme=light&` +
 			`refresh=${ this.refreshRateGraphs }`);
 	}
